2a4b0b422eda8d6e148a35fd693897cef80c3749
Ishita Kundaliya <kundaliyaishita@gmail.com>
2025-06-08 15:05:40 -0500

    adding learn-spring

---
+++ learn-spring/src/main/java/com/spring/learn_spring/LearnSpringApplication.java
@@ -0,0 +1,22 @@
+package com.spring.learn_spring;
+
+import org.springframework.boot.SpringApplication;
+import com.spring.learn_spring.game.GameRunner;
+import com.spring.learn_spring.game.PacmanGame;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class LearnSpringApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringApplication.class, args);
+//		PacmanGame game = new PacmanGame();
+//		GameRunner runner = new GameRunner(game);
+		GameRunner runner = context.getBean(GameRunner.class);
+		runner.run();
+
+	}
+
+}
---
+++ learn-spring/src/main/java/com/spring/learn_spring/game/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.spring.learn_spring.game;
+
+public interface GamingConsole {
+    void up();
+    void down();
+    void left();
+    void right();
+}
