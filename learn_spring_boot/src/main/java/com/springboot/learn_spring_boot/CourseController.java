0b37c305867a57346f888d0471ee2f4a27f64c2d
Ishita Kundaliya <kundaliyaishita@gmail.com>
2025-06-23 16:01:45 -0500

    created first rest api

---
+++ learn_spring_boot/src/main/java/com/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,19 @@
+package com.springboot.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "C1", "A1"),
+                new Course(2, "C2", "A2")
+        );
+    }
+
+}
